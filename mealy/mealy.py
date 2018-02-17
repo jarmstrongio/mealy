a8a5706b182214e81ceef14f4b6031ac3cb214fe
James Armstrong <jarmstrong@Unaha-Closp.local>
2018-02-17 17:00:47 +0000

    Turn mealy into a webservice

--- mealy/mealy.py
+++ mealy/mealy.py
@@ -6,15 +6,32 @@ from pprint import pprint
 from meals_input import read_meals_json
 from meals import generate_meal_list
 from ingredients import generate_shopping_list
+from view import render
+
+import web
+
+urls = (
+    '/', 'display',
+    '/raw', 'raw'
+)
+
+class raw:
+    def GET(self):
+        MEALS = read_meals_json()
+        MEAL_LIST = generate_meal_list(MEALS)
+        SHOPPING_LIST = generate_shopping_list(MEAL_LIST)
+
+        return render.raw(MEAL_LIST)
+
+class display:
+    def GET(self):
+        MEALS = read_meals_json()
+        MEAL_LIST = generate_meal_list(MEALS)
+        SHOPPING_LIST = generate_shopping_list(MEAL_LIST)
+
+        return render.meals(MEAL_LIST)
 
 if __name__ == "__main__":
-    MEALS = read_meals_json()
-    MEAL_LIST = generate_meal_list(MEALS)
-    SHOPPING_LIST = generate_shopping_list(MEAL_LIST)
-
-    print('Meal list')
-    for meal in MEAL_LIST:
-        print(meal['name'])
-    print('')
-    print('Shopping list')
-    pprint(SHOPPING_LIST)
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
+    app.run()
---
+++ mealy/view.py
@@ -0,0 +1,8 @@
+import web
+import config
+
+t_globals = dict(
+  datestr=web.datestr,
+)
+render = web.template.render('templates/', cache=config.cache, globals=t_globals)
+render._keywords['globals']['render'] = render
