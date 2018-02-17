c4b3d9278b947906e527c7e539688c9fe33f28b9
James Armstrong <jarmstrong@Unaha-Closp.local>
2018-02-17 17:34:10 +0000

    Add shopping list to raw output

--- mealy/mealy.py
+++ mealy/mealy.py
@@ -2,7 +2,6 @@
 This application reads in a JSON document of possible meals to cook and displays a meal plan for the
 week.
 '''
-from pprint import pprint
 from meals_input import read_meals_json
 from meals import generate_meal_list
 from ingredients import generate_shopping_list
@@ -21,13 +20,14 @@ class raw:
         MEAL_LIST = generate_meal_list(MEALS)
         SHOPPING_LIST = generate_shopping_list(MEAL_LIST)
 
-        return render.raw(MEAL_LIST)
+        result = {'meals':MEAL_LIST, 'shopping':SHOPPING_LIST}
+
+        return render.raw(result)
 
 class display:
     def GET(self):
         MEALS = read_meals_json()
         MEAL_LIST = generate_meal_list(MEALS)
-        SHOPPING_LIST = generate_shopping_list(MEAL_LIST)
 
         return render.meals(MEAL_LIST)
 
