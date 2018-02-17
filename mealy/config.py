2a07fb5aa607685de69829521c9a7f88b70967ee
James Armstrong <jarmstrong@Unaha-Closp.local>
2018-02-17 17:36:10 +0000

    Remove useless config file

--- mealy/config.py
+++
@@ -1,3 +0,0 @@
-import web
-
-cache = False
--- mealy/view.py
+++ mealy/view.py
@@ -1,8 +1,7 @@
 import web
-import config
 
 t_globals = dict(
   datestr=web.datestr,
 )
-render = web.template.render('templates/', cache=config.cache, globals=t_globals)
+render = web.template.render('templates/', cache=False, globals=t_globals)
 render._keywords['globals']['render'] = render
